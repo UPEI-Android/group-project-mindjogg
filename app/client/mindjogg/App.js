beafe7e723044241898b7fe5cdb06686615c8811
Demi Adisa <demiadisa@gmail.com>
2022-01-29 04:39:15 -0400

    Renamed app/client/mindjogg/app to app_elements Included ui_packages.md research in documentation

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -2,6 +2,7 @@ import { React } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Home } from "@material-ui/icons";
+import Button from "./app/components/standard_button";
 
 export default function App() {
   return (
