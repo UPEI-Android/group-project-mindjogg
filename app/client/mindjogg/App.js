bd4e12f8616af986236b66f51c5d3fd31bd95491
ja-chisholm <andrew.chisholm10@gmail.com>
2022-01-30 14:00:04 -0600

    Add stack, tab, and drawer navigation to app

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -2,16 +2,14 @@ import { React } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Home } from "@material-ui/icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to MindJogg!</Text>
-      <Home></Home>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +19,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to MindJogg!</Text>
+      <Home></Home>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
