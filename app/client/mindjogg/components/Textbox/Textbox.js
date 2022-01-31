57e8f560fe048ad262127c29a1c6188f6c3d0cb2
ja-chisholm <andrew.chisholm10@gmail.com>
2022-01-31 11:14:53 -0600

    Create simple textbox with icon

--- app/client/mindjogg/components/Textbox/Textbox.js
+++ app/client/mindjogg/components/Textbox/Textbox.js
@@ -1,9 +1,73 @@
 import React from "react";
+import { View, Image, StyleSheet } from "react-native";
 
-function Textbox() {
+const styles = StyleSheet.create({
+    sectionStyle: {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#fff",
+      borderWidth: 0.5,
+      borderColor: "#000",
+      height: 40,
+      borderRadius: 5,
+      margin: 10,
+    },
+    imageStyle: {
+      padding: 10,
+      margin: 5,
+      height: 25,
+      width: 25,
+      resizeMode: "stretch",
+      alignItems: "center",
+    },
+  });
+
+Textbox.propTypes = {
+    name: React.propTypes.string.isRequired,
+    maxLength: React.propTypes.number,
+    minLength: React.propTypes.number,
+    placeholder: React.propTypes.string,
+    size: React.propTypes.number.isRequired,
+    required: React.propTypes.bool,
+}
+
+Textbox.defaultProps = {
+    mame: "myTextbox",
+    maxLength: 1000,
+    minLength: 1,
+    placeholder: "Input Text Here",
+    size: 20,
+    required: false,
+}
+
+function Textbox(props) {
     return(
-        <Textbox></Textbox>
+        <View style={styles.sectionStyle}>
+            <Image
+            // uri is just for testing
+                source={{uri:"https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png"}}
+                style={styles.imageStyle}
+            >
+            </Image>
+            <input
+                style = {{flex: 1, height: 35, margin: 10, border: 0}}
+                type = "text"
+                onChange = { handleChange }
+                name = { props.name }
+                maxLength = { props.maxLength }
+                minLength = { props.minLength }
+                placeholder = { props.placeholder }
+                size = { props.size }
+                required = { props.required }
+            >
+            </input>
+        </View>
     );
+}
+
+function handleChange() {
+    
 }
 
 export default Textbox;
