e404860f660068d0475377e013232e7bfe153b25
ja-chisholm <andrew.chisholm10@gmail.com>
2022-02-01 20:18:31 -0600

    Add existence test

--- app/client/mindjogg/components/Textbox/Textbox.test.js
+++ app/client/mindjogg/components/Textbox/Textbox.test.js
@@ -1,3 +1,10 @@
-test("it works", () => {
-    expect(true).toBeTruthy();
-});
+import Textbox from "./Textbox";
+import React from "react";
+import renderer from "react-test-renderer";
+
+describe("Textbox", () => {
+    it("should render", () => {
+      const tree = renderer.create(<Textbox />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
