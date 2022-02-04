442a3296dec6976557c6839d5f530d1f6427e99a
Demi Adisa <demiadisa@gmail.com>
2022-02-04 01:28:24 -0400

    File Changes

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -1,27 +1,23 @@
 import { React } from "react";
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
 // import { Home } from "@material-ui/icons";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Button } from "react-native-paper";
 //import { Text } from "react-native-paper/lib/typescript/components/Avatar/Avatar";
-=======
+
 //import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
->>>>>>> fa5c28b524a430eff384baf03b09364e97319e64
 
-
-
+import PositveJournal from "./screens/navigation/PositiveJournalStackNavigator";
 import SmartGoalStackNavigator from "./screens/navigation/SmartGoalStackNavigator";
 
 function App() {
   return (
     <NavigationContainer>
-      <SmartGoalStackNavigator />
+      <PositveJournal />
     </NavigationContainer>
   );
 }
-
 
 export default App;
