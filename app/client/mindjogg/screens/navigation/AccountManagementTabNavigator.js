42db5729801b3f8c2f6d265158c2070e31645214
sontushkp <spaul4298@upei.ca>
2022-02-04 21:02:03 -0400

    import react in tab navigator file

--- app/client/mindjogg/screens/navigation/AccountManagementTabNavigator.js
+++ app/client/mindjogg/screens/navigation/AccountManagementTabNavigator.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
