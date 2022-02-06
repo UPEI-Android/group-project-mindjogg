d3d7b8d9b544d04244278bb8276af094b0251dd0
ja-chisholm <andrew.chisholm10@gmail.com>
2022-02-06 12:03:01 -0600

    Resolve conflicts in App.js

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -1,25 +1,18 @@
 import { React } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Textbox } from "./components/Textbox";
-// import { Home } from "@material-ui/icons";
+//import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+
+
+import AccountManagementTabNavigator from "./screens/navigation/AccountManagementTabNavigator";
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to MindJogg!</Text>
-      {/* <Home></Home> this home icon is breaking the app in both android and ios */}
-      <StatusBar style="auto" />
-      <Textbox />
-    </View>
+    <NavigationContainer>
+      <AccountManagementTabNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+export default App;
