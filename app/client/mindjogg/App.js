82bcfcba47b18bb7327fc6f03552d938fe866a4d
Demi Adisa <demiadisa@gmail.com>
2022-02-07 08:27:23 -0400

    Merge with Dev

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -10,15 +10,10 @@ import { Provider as PaperProvider } from "react-native-paper";
 //import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 
-<<<<<<< HEAD
-import PositveJournal from "./screens/navigation/PositiveJournalStackNavigator";
-import SmartGoalStackNavigator from "./screens/navigation/SmartGoalStackNavigator";
-=======
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Drawer = createDrawerNavigator();
 
->>>>>>> 2fa9c5acd5a02dbc24105740e6bbc2b6c2996953
 import AccountManagementTabNavigator from "./screens/navigation/AccountManagementTabNavigator";
 import EmergencySupportTabNavigator from "./screens/navigation/EmergencySupportTabNavigator";
 import PositiveJournalTabNavigator from "./screens/navigation/PositiveJournalTabNavigator";
@@ -27,16 +22,21 @@ import SmartGoalTabNavigator from "./screens/navigation/SmartGoalTabNavigator";
 function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <PositveJournal />
-=======
-      <Drawer.Navigator screenOptions={{headerShown: false}}>
-        <Drawer.Screen name="AccountManagement" component={AccountManagementTabNavigator} />
-        <Drawer.Screen name="EmergencySupport" component={EmergencySupportTabNavigator} />
-        <Drawer.Screen name="PositiveJournal" component={PositiveJournalTabNavigator} />
+      <Drawer.Navigator screenOptions={{ headerShown: false }}>
+        <Drawer.Screen
+          name="AccountManagement"
+          component={AccountManagementTabNavigator}
+        />
+        <Drawer.Screen
+          name="EmergencySupport"
+          component={EmergencySupportTabNavigator}
+        />
+        <Drawer.Screen
+          name="PositiveJournal"
+          component={PositiveJournalTabNavigator}
+        />
         <Drawer.Screen name="SmartGoal" component={SmartGoalTabNavigator} />
       </Drawer.Navigator>
->>>>>>> 2fa9c5acd5a02dbc24105740e6bbc2b6c2996953
     </NavigationContainer>
   );
 }
