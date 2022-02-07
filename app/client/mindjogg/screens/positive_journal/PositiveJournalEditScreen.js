752f45e6dcd26890794cde8efcc55f69baaa8ca8
sontushkp <spaul4298@upei.ca>
2022-02-07 13:19:38 -0400

    added test stuite for  positive journal app

--- app/client/mindjogg/screens/positive_journal/PositiveJournalEditScreen.js
+++ app/client/mindjogg/screens/positive_journal/PositiveJournalEditScreen.js
@@ -5,7 +5,7 @@ const PositiveJournalEditScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
-        This is the PositiveJournalEditScreen.
+        Edit Positive Journal
       </Text>
     </View>
   );
