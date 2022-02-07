ed6e8cb744d68476339ffa0999c511c808a44ed7
Demi Adisa <demiadisa@gmail.com>
2022-02-07 08:24:06 -0400

    Updates to App.js

--- app/client/mindjogg/App.js
+++ app/client/mindjogg/App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from "react-native";
 //import { Textbox } from "./components/Textbox";
 // import { Home } from "@material-ui/icons";
 import { Provider as PaperProvider } from "react-native-paper";
-import { Button } from "react-native-paper";
 //import { Text } from "react-native-paper/lib/typescript/components/Avatar/Avatar";
 
 //import { StatusBar } from "expo-status-bar";
