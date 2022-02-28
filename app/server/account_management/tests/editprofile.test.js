07e9794e760b69388a24dd2a25f6568c41a8eb06
jyaneshseeam <55171030+jyaneshseeam@users.noreply.github.com>
2022-02-28 10:03:29 -0400

    tests now work

--- app/server/account_management/tests/editprofile.test.js
+++ app/server/account_management/tests/editprofile.test.js
@@ -4,8 +4,9 @@ const database= require("../../database/database_connection");
 
 
 describe("POST /users/edit/personalinfo", () => {
-    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjFjZDA0NTRhZjg4MjE2Y2ExOTkzZWUiLCJpYXQiOjE2NDYwNTU1MTF9.juyNwOQ1HEHPYSesmxQJ88NUdg2fmR7mQ_CdtU7DOt0";        test("should respond with a 200 status code for updated contact user info", async () => {
-        const response = await request(app).post("/users/edit/personalinfo").set("auth-token", token).send({
+    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjFjZDA0NTRhZjg4MjE2Y2ExOTkzZWUiLCJpYXQiOjE2NDYwNTU1MTF9.juyNwOQ1HEHPYSesmxQJ88NUdg2fmR7mQ_CdtU7DOt0";     
+       test("should respond with a 200 status code for updated contact user info", async () => {
+        const response = await request(app).patch("/users/edit/personalinfo").set("auth-token", token).send({
             //Using exsiting user test data
             "userFirstName": "Roshan",
             "userMiddleName": Math.random().toString(36).substring(2,7),
@@ -16,7 +17,7 @@ describe("POST /users/edit/personalinfo", () => {
     })
 
     test("should respond with a 200 status code for updated contact user info", async () => {
-        const response = await request(app).post("/users/edit/contactinfo").set("auth-token", token).send({
+        const response = await request(app).patch("/users/edit/contactinfo").set("auth-token", token).send({
             //Using exsiting user test data
             "userEmail": Math.random().toString(36).substring(2,7)+"@fae.com",
             "userPhone": "999-999-9999"
