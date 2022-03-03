492c6ffc874c1275f8da21ef569ffa0bbd0a49dd
sontushkp <spaul4298@upei.ca>
2022-03-03 14:59:57 -0400

    editted account management stack navigator to pass the tests more

--- app/client/mindjogg/tests/navigation/AccountManagementStackNavigator.test.js
+++ app/client/mindjogg/tests/navigation/AccountManagementStackNavigator.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import { NavigationContainer } from "@react-navigation/native";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render } from "@testing-library/react-native";
 
 // import the component that we want to test
 //import AuthenticationStackNavigator from "../../screens/navigation/AuthenticationStackNavigator";
@@ -21,6 +21,7 @@ describe("Testing react navigation", () => {
     const { findByText } = render(component);
 
     const getStarted = await findByText("Get Started");
+    expect(getStarted).toBeTruthy();
     // fireEvent.press(getStarted);
 
     // const signInHeader = await findByText("Please Sign In to Continue");
