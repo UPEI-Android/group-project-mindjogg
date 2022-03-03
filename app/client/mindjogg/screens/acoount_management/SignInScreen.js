ec1428971de723d861657be4472a45a80a813b80
sontushkp <spaul4298@upei.ca>
2022-03-03 01:56:57 -0400

    Sign In signup screen UI is complete but form submission and validation is still needed

--- app/client/mindjogg/screens/acoount_management/SignInScreen.js
+++ app/client/mindjogg/screens/acoount_management/SignInScreen.js
@@ -1,29 +1,213 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import StdButton from "../../components/StdButton/StdButton";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Platform,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar,
+} from "react-native";
+
 import { globalStyles } from "../../styles/global";
 
+import * as Animatable from "react-native-animatable";
+import StdButton from "../../components/StdButton/StdButton";
+
+import Ionicon from "react-native-vector-icons/Ionicons";
+
 const SignInScreen = ({ navigation }) => {
+  const [data, setData] = React.useState({
+    email: "",
+    password: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+  });
+
+  const handlePasswordChange = (val) => {
+    setData({
+      ...data,
+      password: val,
+    });
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
   return (
-    <View style={[styles.center,globalStyles.pinkBackground]}>
-      <Text>Please Sign In to Continue</Text>
+    <View style={[styles.container, globalStyles.pinkBackground]}>
+      <StatusBar backgroundColor="#FFE3FF" barStyle="dark-content" />
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.header}>
+          <View style={styles.logo_container}>
+            <Animatable.Image
+              animation="bounceIn"
+              duration={1500}
+              source={require("../../assets/Getting_started_logo.png")}
+              style={styles.logo}
+              resizeMode="stretch"
+            />
+            <Text style={styles.text_header}>Welcome Back</Text>
+            <Text style={styles.text_header_Title}>
+              Please Sign In to Continue
+            </Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
 
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View>
+              <View style={styles.action}>
+                <Ionicon name="person-outline" color="#000000" size={20} />
+                <TextInput
+                  placeholder="Email Or Username"
+                  style={styles.textInput}
+                  autoCapitalize="none"
+                />
+              </View>
+              <View style={{ marginTop: 30 }}>
+                <View style={styles.action}>
+                  <Ionicon
+                    name="lock-closed-outline"
+                    color="#000000"
+                    size={20}
+                  />
+                  <TextInput
+                    placeholder="Password"
+                    secureTextEntry={data.secureTextEntry ? true : false}
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    onChangeText={(val) => handlePasswordChange(val)}
+                  />
+                  <TouchableOpacity onPress={updateSecureTextEntry}>
+                    {data.secureTextEntry ? (
+                      <Ionicon name="eye-off-outline" color="grey" size={20} />
+                    ) : (
+                      <Ionicon name="eye-outline" color="grey" size={20} />
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </View>
 
-      <StdButton
-        text="Not a Member? Sign Up"
-        uppercaseOn={false}
-        buttonPress={() => navigation.push("SignUpScreen")}
-      />
+              <View style={styles.button}>
+                <StdButton text="Sign In" buttonPress={() => {}} />
+              </View>
+
+              <View style={styles.signUpButton}>
+                <TouchableOpacity
+                  onPress={() => navigation.push("SignUpScreen")}
+                  style={{ marginBottom: 15 }}
+                >
+                  <Text style={styles.text_hightlight}>Forgot Password?</Text>
+                </TouchableOpacity>
+                <Text style={styles.text_footer}>Not a Member?</Text>
+                <TouchableOpacity
+                  onPress={() => navigation.push("SignUpScreen")}
+                >
+                  <Text style={styles.text_hightlight}>Sign up</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </Animatable.View>
     </View>
   );
 };
 
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.17;
+
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#009387",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 30,
+  },
+  logo_container: {
     alignItems: "center",
-    textAlign: "center",
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+  footer: {
+    flex: 1.5,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: "#977AB7",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  text_header_Title: {
+    color: "#683795",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginTop: 10,
+  },
+  text_footer: {
+    color: "#683795",
+    fontSize: 15,
+  },
+  text_hightlight: {
+    color: "red",
+    fontSize: 15,
+    fontWeight: "bold",
+    marginLeft: 5,
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  errorMsg: {
+    color: "#FF0000",
+    fontSize: 14,
+  },
+  button: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+  signUpButton: {
+    alignItems: "center",
+    marginTop: 15,
   },
 });
 
--- app/client/mindjogg/screens/acoount_management/SignUpScreen.js
+++ app/client/mindjogg/screens/acoount_management/SignUpScreen.js
@@ -1,144 +1,225 @@
 import React from "react";
-import { Text, View, StyleSheet, Dimensions, Platform} from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Platform,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar,
+} from "react-native";
+
 import { globalStyles } from "../../styles/global";
 
 import * as Animatable from "react-native-animatable";
-
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-
-import TextBox from "../../components/Textbox/Textbox";
 import StdButton from "../../components/StdButton/StdButton";
 
-const SignUpScreen = () => {
+import Ionicon from "react-native-vector-icons/Ionicons";
+
+const SignInScreen = ({ navigation }) => {
+  const [data, setData] = React.useState({
+    email: "",
+    password: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+  });
+
+  const handlePasswordChange = (val) => {
+    setData({
+      ...data,
+      password: val,
+    });
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>header</Text>
-        
-      </View>
-      <View style={styles.footer}>
-        <TextBox 
-        name="first_name"
-        placeholder="First Name"
-        height={Dimensions.get("window").height * 0.08}
-        width={Dimensions.get("window").width - 50}
-        required={true}
-        icon={<Icon name="account" size={30} color="black" />}
-        />
-        <TextBox 
-        name="first_name"
-        placeholder="Last Name"
-        height={Dimensions.get("window").height * 0.08}
-        width={Dimensions.get("window").width - 50}
-        required={true}
-        icon={<Icon name="account" size={30} color="black" />}
-        />
-        <TextBox 
-        name="email"
-        placeholder="Email"
-        height={Dimensions.get("window").height * 0.08}
-        width={Dimensions.get("window").width - 50}
-        required={true}
-        icon={<Icon name="email" size={30} color="black" />}
-        />
-        <TextBox 
-        name="email"
-        placeholder="Email"
-        height={Dimensions.get("window").height * 0.08}
-        width={Dimensions.get("window").width - 50}
-        required={true}
-        icon={<Icon name="email" size={30} color="black" />}
-        />
-        <View style={styles.button}>
-          <StdButton 
-            text="Sign Up"
-            buttonPress={() => {}}
-          />
+    <View style={[styles.container, globalStyles.pinkBackground]}>
+      <StatusBar backgroundColor="#FFE3FF" barStyle="dark-content" />
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.header}>
+          <Text style={styles.text_header_Title}>
+            Create an account to continue
+          </Text>
         </View>
-        <View style={styles.signInButton}>
-          <Text style={styles.text_footer}>Already have an account?</Text>
-          <Text style={styles.text_signIn}>Sign In</Text>
-        </View>
-      </View>
+      </TouchableWithoutFeedback>
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View>
+              <View style={styles.action}>
+                <Ionicon name="person-outline" color="#000000" size={20} />
+                <TextInput
+                  placeholder="First Name"
+                  style={styles.textInput}
+                  autoCapitalize="none"
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <View style={styles.action}>
+                  <Ionicon name="person-outline" color="#000000" size={20} />
+                  <TextInput
+                    placeholder="Last Name"
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <View style={styles.action}>
+                  <Ionicon name="at-outline" color="#000000" size={20} />
+                  <TextInput
+                    placeholder="Username"
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <View style={styles.action}>
+                  <Ionicon name="mail-outline" color="#000000" size={20} />
+                  <TextInput
+                    placeholder="Email"
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <View style={styles.action}>
+                  <Ionicon
+                    name="lock-closed-outline"
+                    color="#000000"
+                    size={20}
+                  />
+                  <TextInput
+                    placeholder="Password"
+                    secureTextEntry={data.secureTextEntry ? true : false}
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    onChangeText={(val) => handlePasswordChange(val)}
+                  />
+                  <TouchableOpacity onPress={updateSecureTextEntry}>
+                    {data.secureTextEntry ? (
+                      <Ionicon name="eye-off-outline" color="grey" size={20} />
+                    ) : (
+                      <Ionicon name="eye-outline" color="grey" size={20} />
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              <View style={styles.button}>
+                <StdButton text="Sign Up" buttonPress={() => {}} />
+              </View>
+
+              <View style={styles.signUpButton}>
+                <Text style={styles.text_footer}>Already a Member?</Text>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                  <Text style={styles.text_hightlight}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </Animatable.View>
     </View>
   );
-}
+};
 
-export default SignUpScreen;
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.14;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: globalStyles.pink.color,
+    flex: 1,
+    backgroundColor: "#009387",
   },
   header: {
-      flex: 1,
-      justifyContent: "flex-end",
-      paddingHorizontal: 20,
-      paddingBottom: 50
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  logo_container: {
+    alignItems: "center",
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
   },
   footer: {
-      flex: Platform.OS === "ios" ? 3 : 3,
-      backgroundColor: "#fff",
-      borderTopLeftRadius: 30,
-      borderTopRightRadius: 30,
-      paddingHorizontal: 20,
-      paddingVertical: 30
+    flex: 3,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
   text_header: {
-      color: "#fff",
-      fontWeight: "bold",
-      fontSize: 30
+    color: "#977AB7",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  text_header_Title: {
+    color: "#683795",
+    fontWeight: "bold",
+    fontSize: 28,
+    marginTop: 10,
   },
   text_footer: {
-      color: globalStyles.purple.color,
-      fontSize: 18,
-      fontWeight: "bold",
+    color: "#683795",
+    fontSize: 15,
   },
-  text_signIn: {
+  text_hightlight: {
     color: "red",
-    fontSize: 18,
+    fontSize: 15,
     fontWeight: "bold",
-    marginLeft: 5
-},
+    marginLeft: 5,
+  },
   action: {
-      flexDirection: "row",
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: "#f2f2f2",
-      paddingBottom: 5
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
   },
   textInput: {
-      flex: 1,
-      marginTop: Platform.OS === "ios" ? 0 : -12,
-      paddingLeft: 10,
-      color: "#05375a",
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  errorMsg: {
+    color: "#FF0000",
+    fontSize: 14,
   },
   button: {
-      alignItems: "center",
-      marginTop: 15
+    alignItems: "center",
+    marginTop: 45,
   },
-  signInButton: {
+  signUpButton: {
     alignItems: "center",
     marginTop: 15,
   },
-  signIn: {
-      width: "100%",
-      height: 50,
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 10
-  },
-  textSign: {
-      fontSize: 18,
-      fontWeight: "bold"
-  },
-  textPrivate: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-      marginTop: 20
-  },
-  color_textPrivate: {
-      color: "grey"
-  }
 });
+
+export default SignInScreen;
