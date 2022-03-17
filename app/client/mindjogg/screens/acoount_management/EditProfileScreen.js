1094382ba558ad8d898713fca8e64f53a1f216b4
ja-chisholm <andrew.chisholm10@gmail.com>
2022-03-17 13:10:52 -0500

    Add all information to screen (no styling)

--- app/client/mindjogg/screens/acoount_management/EditProfileScreen.js
+++ app/client/mindjogg/screens/acoount_management/EditProfileScreen.js
@@ -1,26 +1,104 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 import StdButton from "../../components/StdButton/StdButton";
-
+import { globalStyles } from "../../styles/global";
+import { Avatar, Title, Caption } from "react-native-paper";
 
 const ProfileScreen = ({ navigation }) => {
   return (
-    <View style={styles.center}>
-      <Text> Edit Profile</Text>
+    <View style={[styles.container, globalStyles.pinkBackground]}>
+
+    <View style={styles.userInfoSection}>
+      <View style={{ flexDirection: "row", marginTop: 15 }}>
+        <Avatar.Image
+          source={{
+            uri: "https://images.saymedia-content.com/.image/t_share/MTc2NDU3MDE0NzExNjkwNDUz/about-steve-jobs.jpg",
+          }}
+            size={70}
+        />
+        <View style={{ marginLeft: 15, flexDirection: "column" }}>
+          <Title style={styles.title}>Steve Jobs</Title>
+              <Caption style={styles.caption}>@stevejobs</Caption>
+        </View>
+      </View>
+    </View>
+
+
       <StdButton
-        text="Cancel"
-        buttonPress={() => navigation.goBack()}
+        text="Personal Information"
+        uppercaseOn={false}
+        buttonPress={() => {}}
+      />
+      <StdButton
+        text="Contact Information"
+        uppercaseOn={false}
+        buttonPress={() => {}}
+      />
+      <StdButton
+        text="Emergency Contacts"
+        uppercaseOn={false}
+        buttonPress={() => {}}
+      />
+      <StdButton
+        text="Update Password"
+        uppercaseOn={false}
+        buttonPress={() => {}}
+      />
+
+      <StdButton
+        text="Logout"
+        uppercaseOn={false}
+        buttonPress={() => {}}
       />
     </View>
   );
 };
 
+const { height } = Dimensions.get("screen");
+
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#009387",
+  },
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: "row",
     alignItems: "center",
-    textAlign: "center",
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 15,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  bottomDrawerSection: {
+    marginBottom: 15,
+    borderTopColor: "#f4f4f4",
+    borderTopWidth: 1,
+  },
+  preference: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
   },
 });
 
