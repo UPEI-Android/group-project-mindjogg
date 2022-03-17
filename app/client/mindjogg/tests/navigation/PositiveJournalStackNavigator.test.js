d940571396a09b559357a7c5f30897841aac1405
jyaneshseeam <55171030+jyaneshseeam@users.noreply.github.com>
2022-03-17 20:15:07 -0300

    Update PositiveJournalStackNavigator.test.js

--- app/client/mindjogg/tests/navigation/PositiveJournalStackNavigator.test.js
+++ app/client/mindjogg/tests/navigation/PositiveJournalStackNavigator.test.js
@@ -1,10 +1,4 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { render, fireEvent } from "@testing-library/react-native";
 
-
-// import the component that we want to test
-import PositiveJournalStackNavigator from "../../screens/navigation/PositiveJournalStackNavigator";
 
 // Use this instead with React Native >= 0.64
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
