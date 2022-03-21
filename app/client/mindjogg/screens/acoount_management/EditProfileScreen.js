15054ffb27c7301a9c4777a504348fb941f3e837
ja-chisholm <andrew.chisholm10@gmail.com>
2022-03-21 07:13:02 -0500

    add logout functionality

--- app/client/mindjogg/screens/acoount_management/EditProfileScreen.js
+++ app/client/mindjogg/screens/acoount_management/EditProfileScreen.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
-import StdButton from "../../components/StdButton/StdButton";
-import { globalStyles } from "../../styles/global";
+/* REACT IMPORTS */
+import { React, useContext } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
 import { Avatar, Title, Caption } from "react-native-paper";
 
+/* LOCAL IMPORTS */
+import StdButton from "../../components/StdButton/StdButton";
+import { globalStyles } from "../../styles/global";
+import { AuthContext } from "../../components/conext/authenticationContext";
+
 const ProfileScreen = ({ navigation }) => {
+  const { signOut } = useContext(AuthContext);
+
+  const signOutHandler = () => { 
+    signOut();
+  }
+
   return (
     <View style={[styles.container, globalStyles.pinkBackground]}>
 
     <View style={styles.userInfoSection}>
-      <View style={{ flexDirection: "row", marginTop: 15 }}>
+      <View style={styles.avatarStyle}>
         <Avatar.Image
           source={{
             uri: "https://images.saymedia-content.com/.image/t_share/MTc2NDU3MDE0NzExNjkwNDUz/about-steve-jobs.jpg",
           }}
             size={70}
         />
-        <View style={{ marginLeft: 15, flexDirection: "column" }}>
-          <Title style={styles.title}>Steve Jobs</Title>
-          <Caption style={styles.caption}>@stevejobs</Caption>
+        <View style={styles.userInfo}>
+          <Title style={styles.name}>Steve Jobs</Title>
+          <Caption style={styles.username}>@stevejobs</Caption>
         </View>
       </View>
     </View>
@@ -60,7 +70,7 @@ const ProfileScreen = ({ navigation }) => {
       <StdButton
         text="Logout"
         uppercaseOn={false}
-        buttonPress={() => {}}
+        buttonPress={() => {signOutHandler()}}
     />
     </View>
     </View>
@@ -78,20 +88,28 @@ const styles = StyleSheet.create({
   userInfoSection: {
     paddingLeft: 20,
   },
+  userInfo: {
+    flexDirection: "column",
+    marginTop: height * 0.01,
+    paddingLeft: 10,
+  },
+  avatarStyle: {
+    flexDirection: "row", 
+    marginTop: height * 0.01,
+  },
   buttonSection: {
-    margin: 5,
     paddingLeft: 20,
   },
   button: {
     margin: 5,
     borderRadius: 30,
   },
-  title: {
+  name: {
     fontSize: 16,
     marginTop: 3,
     fontWeight: "bold",
   },
-  caption: {
+  username: {
     fontSize: 14,
     lineHeight: 14,
   },
