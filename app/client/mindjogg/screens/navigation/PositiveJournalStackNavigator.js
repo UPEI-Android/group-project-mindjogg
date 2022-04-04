c2cadb3f82506ea8f0c153a4e6901a45bdf30a2b
sontushkp <spaul4298@upei.ca>
2022-04-04 02:41:56 -0300

    redesigned posiive journal edit and main screen

--- app/client/mindjogg/screens/navigation/PositiveJournalStackNavigator.js
+++ app/client/mindjogg/screens/navigation/PositiveJournalStackNavigator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View} from "react-native";
+import { View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { globalStyles } from "../../styles/global";
 import Icon from "react-native-vector-icons/Ionicons";
@@ -9,44 +9,55 @@ import PositiveJournalMainScreen from "../positive_journal/PositiveJournalMainSc
 import PositiveJournalEditScreen from "../positive_journal/PositiveJournalEditScreen";
 import PositiveJournalDetailScreen from "../positive_journal/PositiveJournalDetailScreen";
 
-
 const Stack = createNativeStackNavigator();
 
-const PositiveJournalStackNavigator = ({navigation}) => {
+const PositiveJournalStackNavigator = ({ navigation }) => {
   return (
-
     <View style={{ flex: 1 }} collapsable={false}>
-    <Stack.Navigator initialRouteName="EmergencySupportMainScreen"
-    screenOptions={{
-        headerTitleAlign: "center",
-        headerStyle:[ globalStyles.purpleBackground],
-        headerTintColor: "white",
-        headerBackTitle: "Back",
-      }}
-    >
-        <Stack.Screen name="PositiveJournalMainScreen" component={PositiveJournalMainScreen} options = {
-          {
+      <Stack.Navigator
+        initialRouteName="EmergencySupportMainScreen"
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerStyle: [globalStyles.purpleBackground],
+          headerTintColor: "white",
+          headerBackTitle: "Back",
+        }}
+      >
+        <Stack.Screen
+          name="PositiveJournalMainScreen"
+          component={PositiveJournalMainScreen}
+          options={{
             title: "Positive Journal",
             headerLeft: () => (
-              <Icon.Button name="menu" size={25} style={globalStyles.purpleBackground} onPress={() => {navigation.openDrawer()}}></Icon.Button>
-            )
-          }
-        }/>
-        <Stack.Screen name="PositiveJournalDetailScreen" component={PositiveJournalDetailScreen} options = {
-          {
+              <Icon.Button
+                name="menu"
+                size={25}
+                style={globalStyles.purpleBackground}
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+              ></Icon.Button>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="PositiveJournalDetailScreen"
+          component={PositiveJournalDetailScreen}
+          options={{
             title: "Positive Journal details",
-          }
-        }/>
+          }}
+        />
 
-          <Stack.Screen name="PositiveJournalEditScreen" component={PositiveJournalEditScreen} options = {
-              {
-                title: "Positive Journal Edit",
-              }
-            }/>
-            
-    </Stack.Navigator>
+        <Stack.Screen
+          name="PositiveJournalEditScreen"
+          component={PositiveJournalEditScreen}
+          options={{
+            title: "Positive Journal Edit",
+          }}
+        />
+      </Stack.Navigator>
     </View>
   );
-}
+};
 
 export default PositiveJournalStackNavigator;
--- app/client/mindjogg/screens/positive_journal/PositiveJournalEditScreen.js
+++ app/client/mindjogg/screens/positive_journal/PositiveJournalEditScreen.js
@@ -1,107 +1,150 @@
-import React,{useState} from "react";
-import { TextInput, View,StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import StdButton from "../../components/StdButton/StdButton";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import  {Picker}  from "@react-native-picker/picker"
+import { Picker } from "@react-native-picker/picker";
 import { globalStyles } from "../../styles/global";
 import axios from "axios";
 
-const backend = "http://192.168.2.35:8080";
+const backend = "http://192.168.2.14:8080";
 
+const PositiveJournalEditScreen = ({ navigation }) => {
+  const [journalEntry, setJournalEntry] = useState("");
+  const [title, setJournalTitle] = useState("");
+  const [type, setJournalType] = useState("Travel");
 
-
-const PositiveJournalEditScreen = ({navigation}) => {
-  const [journalEntry,setJournalEntry]=useState("");
-  const [title,setJournalTitle]=useState("");
-  const [type,setJournalType]=useState("Travel");
-
-
-  const addNewEntry= async()=>{
- 
+  const addNewEntry = async () => {
     const userToken = await AsyncStorage.getItem("userToken");
     const data = JSON.stringify({
       type: type,
       title: title,
       entry: journalEntry,
     });
-  
-     await axios.post(backend + "/users/addJournalEntry", data, {
-      headers: { "Content-Type": "application/json",  "auth-token":userToken},
+
+    await axios.post(backend + "/users/addJournalEntry", data, {
+      headers: { "Content-Type": "application/json", "auth-token": userToken },
     });
-  }
+  };
 
   return (
-    <View style={[{ flex: 1, justifyContent: "center", alignItems: "center" },globalStyles.pinkBackground]}>
-      <View  style={{flexDirection:"row",justifyContent: "space-between"}}>
-    
-      <TextInput 
-        style={styles.title} 
-        textAlignVertical="top"
-        placeholder="Enter Title"
-        onChangeText={(title)=> setJournalTitle(title)}
-      />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={[{ flex: 1 }, globalStyles.pinkBackground]}>
+        <View style={{ justifyContent: "space-between" }}>
+          <TextInput
+            style={styles.journalTitle}
+            textAlignVertical="top"
+            placeholder="Enter Title"
+            onChangeText={(title) => setJournalTitle(title)}
+          />
+          <View style={styles.categoryContainer}>
+            <Text style={styles.categoryText}>Category : </Text>
+            <Picker
+              selectedValue={type}
+              style={{
+                height: 1,
+                width: 250,
+                marginTop: 10,
+                marginLeft: Dimensions.get("window").width * 0.01,
+                marginBottom: 5,
+              }}
+              itemStyle={{ fontSize: 50 }}
+              onValueChange={(itemValue) => setJournalType(itemValue)}
+            >
+              <Picker.Item style={styles.categoryText} label="None" value="" />
+              <Picker.Item
+                style={styles.categoryText}
+                label="Travel"
+                value="Travel"
+              />
+              <Picker.Item
+                style={styles.categoryText}
+                label="Personal"
+                value="Personal"
+              />
+              <Picker.Item
+                style={styles.categoryText}
+                label="Work"
+                value="Work"
+              />
+              <Picker.Item
+                style={styles.categoryText}
+                label="Life"
+                value="Life"
+              />
+            </Picker>
+          </View>
+        </View>
+        <TextInput
+          style={styles.journalBody}
+          textAlignVertical="top"
+          placeholder="Enter your thoughts..."
+          multiline={true}
+          scrollEnabled={true}
+          onChangeText={(entry) => setJournalEntry(entry)}
+        />
 
-      <Picker
-        selectedValue={type}
-        style={{ height: 50, width: 130  }}
-        onValueChange={(itemValue) => setJournalType(itemValue)}
-      >
-        <Picker.Item label="Travel" value="Travel" />
-        <Picker.Item label="Personal" value="Personal" />
-        <Picker.Item label="Work" value="Work" />
-        <Picker.Item label="Life" value="Life" />
-        </Picker>
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 15,
+            justifyContent: "center",
+          }}
+        >
+          <StdButton
+            text={"Cancel"}
+            buttonColour={"#9B7FBA"}
+            buttonWidth={150}
+            buttonPress={() => navigation.push("PositiveJournalMainScreen")}
+          />
 
+          <View style={{ margin: Dimensions.get("window").width * 0.1 }}></View>
+          <StdButton
+            text={"Save"}
+            buttonColour={"#663591"}
+            buttonWidth={125}
+            buttonPress={() => {
+              addNewEntry();
+              navigation.push("PositiveJournalMainScreen", journalEntry);
+            }}
+          />
+        </View>
       </View>
-      <TextInput 
-        style={styles.input} 
-        textAlignVertical="top"
-        placeholder="Enter your thoughts..."
-        onChangeText={(entry)=> setJournalEntry(entry)}
-      />
-
-      <View style={{flexDirection: "row"}}>
-        <StdButton
-        text={"Cancel"}
-        buttonColour={"#9B7FBA"}
-        buttonWidth={125}
-        buttonPress={() =>    navigation.push("PositiveJournalMainScreen")}
-      />
-      
-      <View style={{margin:10}}></View>
-      <StdButton
-        text={"Add Entry"}
-        buttonColour={"#663591"}
-        buttonWidth={125}
-        buttonPress={() => 
-          {addNewEntry(); navigation.push("PositiveJournalMainScreen",journalEntry);}
-      }
-      />
-      </View>
-      
-    </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
-    input:{
-      borderWidth:1,
-      borderRadius:10,
-      borderColor:"#777",
-      backgroundColor: "#F8F8F8",
-      padding:8,
-      margin:10,
-      width:275,
-      height:400,
-    },
-    title:{
-      borderWidth:1,
-      borderRadius:10,
-      borderColor:"#777",
-      backgroundColor: "#F8F8F8",
-      padding:8,
-      margin:10,
-      width:150,
-    }
+  journalBody: {
+    padding: 8,
+    width: Dimensions.get("window").width * 0.97,
+    height: Dimensions.get("window").height * 0.55,
+    marginLeft: Dimensions.get("window").width * 0.02,
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    marginTop: 5,
+    marginLeft: Dimensions.get("window").width * 0.02,
+    marginBottom: Dimensions.get("window").height * 0.001,
+    alignItems: "center",
+  },
+  categoryText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#9B7FBA",
+  },
+  journalTitle: {
+    padding: 8,
+    marginLeft: Dimensions.get("window").width * 0.015,
+    marginTop: Dimensions.get("window").height * 0.02,
+    width: Dimensions.get("window").width * 0.97,
+  },
 });
 export default PositiveJournalEditScreen;
